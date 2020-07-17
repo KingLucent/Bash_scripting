89351d46947da75a1455181495d36ea70a3118a6
Dmytrii Tykholaz <52358087+KingLucent@users.noreply.github.com>
2020-07-17 17:48:46 +0300

    Finished version

--- s4.sh
+++ s4.sh
@@ -1,10 +1,8 @@
 cp -r /home/d /home/backup
-#diff -qr ./backup/d ./d
 ls /home/backup/d>bp.txt
 ls /home/d>d.txt
 diff bp.txt d.txt >diff.txt
 while read file; do
 cp /home/backup/d/"${file}" home/d
-echo "${file}"
 done < diff.txt
 
