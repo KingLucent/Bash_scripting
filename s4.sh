cbd7ac0f7362a690d90cd4d3c02d5289375e6fa5
Dmytrii Tykholaz <52358087+KingLucent@users.noreply.github.com>
2020-07-17 16:55:15 +0300

    Update s4.sh

--- s4.sh
+++ s4.sh
@@ -1,5 +1,9 @@
-mkdir -p /backup
-mapfile -t arr << (diff -qr /backup /etc)
+
+cp -r ./home/d ./home/d/backup
+mapfile -t arr << "( diff -qr ./backup ./d )"
+#arr = (diff -qr /home/backup /home/d)
 for file in "${arr[@]}" ; 
 do
 cp "${file}" /backup
+done
+
